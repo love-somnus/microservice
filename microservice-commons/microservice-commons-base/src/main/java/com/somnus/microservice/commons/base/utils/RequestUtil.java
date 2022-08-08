682b163c7a6ba5146d0d20b11610e69591d208c6
e <love.nacos@gmail.com>
2022-08-08 18:03:54 +0800

    ❤ 版本升级

--- microservice-commons/microservice-commons-base/src/main/java/com/somnus/microservice/commons/base/utils/RequestUtil.java
+++ microservice-commons/microservice-commons-base/src/main/java/com/somnus/microservice/commons/base/utils/RequestUtil.java
@@ -4,16 +4,23 @@ import com.somnus.microservice.commons.base.constant.GlobalConstant;
 import com.somnus.microservice.commons.base.dto.LoginAuthDto;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.lionsoul.ip2region.xdb.Searcher;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * @author Kevin
@@ -26,6 +33,22 @@ import java.net.UnknownHostException;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RequestUtil {
 
+    private static final String GEOPATH = Thread.currentThread().getContextClassLoader().getResource("ip2region.xdb").getPath();
+
+    private static byte[] vectorIndexBuff;
+
+    private static byte[] contentBuff;
+
+    static {
+        try {
+            // 缓存 VectorIndex 索引
+            vectorIndexBuff = Searcher.loadVectorIndexFromFile(GEOPATH);
+            // 缓存整个 xdb 数据
+            contentBuff = Searcher.loadContentFromFile(GEOPATH);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Gets request.
@@ -132,8 +155,6 @@ public class RequestUtil {
         HttpServletRequest request = getRequest();
         LoginAuthDto loginAuthDto;
         try{
-            String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-
             loginAuthDto = new LoginAuthDto();
         } catch (Exception e){
             log.error("未能正确获取登陆用户信息" , e);
@@ -143,23 +164,72 @@ public class RequestUtil {
     }
 
     /**
-     * Gets login auth dto.
-     *
-     * @return the login auth dto
+     * 每个线程需要单独创建一个独立的 Searcher 对象，但是都共享全局的制度 vIndex 缓存。
+     * @param ip        ip
+     * @return
      */
-//    public static LoginAuthDto getLoginUser() {
-//        LoginAuthDto loginAuthDto;
-//        try{
-//            UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//            List<String> roles = (List)SecurityContextHolder.getContext().getAuthentication().getAuthorities();
-//            loginAuthDto = new LoginAuthDto(0L, userDetails.getUsername(), userDetails.getPassword());
-//            log.info("username:{}---->roles:{}", userDetails.getUsername(), roles);
-//        } catch (Exception e){
-//            log.error("未能正确获取登陆用户信息" , e);
-//            loginAuthDto = new LoginAuthDto(0L, "admin", "非正常用户");
-//        }
-//        return loginAuthDto;
-//    }
+    @SneakyThrows
+    public static List<String> getIpAddress(String ip) {
 
+        // 使用全局的 vIndex 创建带 VectorIndex 缓存的查询对象。
+        Searcher searcher = Searcher.newWithVectorIndex(GEOPATH, vectorIndexBuff);
+
+        long sTime = System.nanoTime();
+
+        String region = searcher.search(ip);
+
+        long cost = TimeUnit.NANOSECONDS.toMicros(System.nanoTime() - sTime);
+
+        log.info("{region: {}, ioCount: {}, took: {} μs}\n", region, searcher.getIOCount(), cost);
+
+        return Arrays.stream(region.split("\\|")).filter(s -> !"0".equals(s)).collect(Collectors.toList());
+    }
+
+    /**
+     * 并发使用，用整个 xdb 数据缓存创建的查询对象可以安全的用于并发，
+     * 也就是你可以把这个 searcher 对象做成全局对象去跨线程访问
+     * @param ip   ip
+     * @return
+     */
+    @SneakyThrows
+    public static List<String> getIpAddress2(String ip) {
+
+        // 使用上述的 contentBuff 创建一个完全基于内存的查询对象。
+        Searcher searcher = Searcher.newWithBuffer(contentBuff);
+
+        long sTime = System.nanoTime();
+
+        String region = searcher.search(ip);
+
+        long cost = TimeUnit.NANOSECONDS.toMicros(System.nanoTime() - sTime);
+
+        log.info("{region: {}, ioCount: {}, took: {} μs}\n", region, searcher.getIOCount(), cost);
+
+        return Arrays.stream(region.split("\\|")).filter(s -> !"0".equals(s)).collect(Collectors.toList());
+    }
+
+    @SneakyThrows
+    public static String getCountry(String ip){
+        if(ip.startsWith("192.168.") || ip.startsWith("172.")){
+            return "局域网";
+        }
+        return getIpAddress(ip).get(0);
+    }
+
+    @SneakyThrows
+    public static String getProvince(String ip) {
+        if(ip.startsWith("192.168.") || ip.startsWith("172.")){
+            return "局域网";
+        }
+        return getIpAddress(ip).get(1);
+    }
+
+    @SneakyThrows
+    public static String getCity(String ip) {
+        if(ip.startsWith("192.168.") || ip.startsWith("172.")){
+            return "局域网";
+        }
+        return getIpAddress(ip).get(2);
+    }
 
 }
