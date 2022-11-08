fd5ab9dca3341b893565b577fe271065c3eb8dea
e <love.nacos@gmail.com>
2022-11-08 13:41:43 +0800

    ❤ 版本升级

--- microservice-autoconfigure/src/main/java/com/somnus/microservice/autoconfigure/proxy/util/ProxyUtil.java
+++ microservice-autoconfigure/src/main/java/com/somnus/microservice/autoconfigure/proxy/util/ProxyUtil.java
@@ -5,16 +5,13 @@ import java.util.stream.Collectors;
 
 /**
  * @author Kevin
- * @packageName com.somnus.microservice.autoconfigure.proxy.util
- * @title: ProxyUtil
- * @description: TODO
  * @date 2019/6/14 9:57
  */
 public class ProxyUtil {
     /**
      * 转换Class数组成字符串格式
-     * @param parameterTypes
-     * @return
+     * @param parameterTypes Class<?>[]
+     * @return String
      */
     public static String toString(Class<?>[] parameterTypes) {
         if (Objects.isEmpty(parameterTypes)) {
@@ -29,8 +26,8 @@ public class ProxyUtil {
 
     /**
      * 转换String数组成字符串格式
-     * @param values
-     * @return
+     * @param values String[]
+     * @return String
      */
     public static String toString(String[] values) {
         if (Objects.isEmpty(values)) {
